581f4f322a93807b971b3edbfd9892e1acf291f5
mcoultas4 <54637293+mcoultas4@users.noreply.github.com>
2020-11-10 17:02:31 -0600

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  ClosetApp
+//
+//  Created by Marissa Coultas on 9/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func facebookTapped(_ sender: Any) {
+    
+        print("Facebook tapped")
+    }
+    
+    @IBAction func googleTapped(_ sender: Any) {
+        
+        print("Google tapped")
+    }
+    
+    @IBAction func emailTapped(_ sender: Any) {
+        print("Email tapped")
+    }
+
+}
+
